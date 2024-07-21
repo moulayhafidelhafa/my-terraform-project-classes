4cb910cf228525b52b722bf6ba1751c888b7bb7e
moulayhafidelhafa <167933385+moulayhafidelhafa@users.noreply.github.com>
2024-07-21 00:17:41 -0500

    main updated

--- docker/td/main.tf
+++ docker/td/main.tf
@@ -1,41 +1,9 @@
-# Define the VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-# Define the subnets
-resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "subnet1"
-  }
-}
-
-resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-2b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "subnet2"
-  }
-}
-
-# Define the ECS task definition
 resource "aws_ecs_task_definition" "application" {
   family                = "application"
   execution_role_arn    = "arn:aws:iam::767397838496:role/ecsTaskExecutionRole"
   task_role_arn         = "arn:aws:iam::767397838496:role/ecsTaskExecutionRole"
-  requires_compatibilities = ["FARGATE"]
-  network_mode          = "awsvpc"
-  cpu                   = "256"
-  memory                = "512"
+  cpu                   = "1824"
+  memory                = "3072"
   container_definitions = <<DEFINITION
 [
   {
@@ -68,30 +36,5 @@ resource "aws_ecs_task_definition" "application" {
   }
 ]
 DEFINITION
-}
-
-# Optional: Security group to allow traffic to the ECS tasks
-resource "aws_security_group" "ecs_security_group" {
-  name        = "ecs_security_group"
-  description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ecs_security_group"
-  }
 }
 
