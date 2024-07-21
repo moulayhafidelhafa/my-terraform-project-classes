347a1caed179078e422b7265d74a63662a1a97ac
moulayhafidelhafa <167933385+moulayhafidelhafa@users.noreply.github.com>
2024-07-21 17:27:19 -0500

    UPDAYED MAIN

--- docker/td/main.tf
+++ docker/td/main.tf
@@ -2,46 +2,47 @@ resource "aws_ecs_task_definition" "application" {
   family                = "application"
   execution_role_arn    = "arn:aws:iam::767397838496:role/ecsTaskExecutionRole"
   task_role_arn         = "arn:aws:iam::767397838496:role/ecsTaskExecutionRole"
-  container_definitions = <<TASK_DEFINITION
   cpu                   = "1024"
   memory                = "3072"
+  container_definitions = <<TASK_DEFINITION
 [
-}
-    "containerDefinitions": [
+    {
+      name: "wordpress",
+      image: "docker.io/wordpress:latest",
+      cpu: 0,
+      portMappings: [
         {
-            "name": "wordppress",
-            "image": "docker.io/wordpress:latest",
-            "cpu": 0,
-            "portMappings": [
-                {
-                    "name": "wordppress-80-tcp",
-                    "containerPort": 80,
-                    "hostPort": 80,
-                    "protocol": "tcp",
-                    "appProtocol": "http"
-                }
-            ],
-            "essential": true,
-            "environment": [],
-            "environmentFiles": [],
-            "mountPoints": [],
-            "volumesFrom": [],
-            "ulimits": [],
-            "logConfiguration": {
-                "logDriver": "awslogs",
-                "options": {
-                    "awslogs-group": "/ecs/application",
-                    "awslogs-create-group": "true",
-                    "awslogs-region": "us-east-2",
-                    "awslogs-stream-prefix": "ecs"
-                },
-                "secretOptions": []
-            },
-            "systemControls": []
+          name: "wordpress-80-tcp",
+          containerPort: 80,
+          hostPort: 80,
+          protocol: "tcp",
+          appProtocol: "http"
         }
+      ],
+      essential: true,
+      environment: [],
+      environmentFiles: [],
+      mountPoints: [],
+      volumesFrom: [],
+      ulimits: [],
+      logConfiguration: {
+        logDriver: "awslogs",
+        options: {
+          "awslogs-group": "/ecs/application",
+          "awslogs-create-group": "true",
+          "awslogs-region": "us-east-2",
+          "awslogs-stream-prefix": "ecs"
+        },
+        secretOptions: []
+      },
+      systemControls: []
+    }
 ]
 TASK_DEFINITION
 }
+
+
+
 
 
 
