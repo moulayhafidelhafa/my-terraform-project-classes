1037fdac029c17ab09d330a3c4394dd0f5e910fe
moulayhafidelhafa <167933385+moulayhafidelhafa@users.noreply.github.com>
2024-07-21 19:23:59 -0500

    updated main

--- docker/td/main.tf
+++ docker/td/main.tf
@@ -4,35 +4,35 @@ resource "aws_ecs_task_definition" "application" {
   task_role_arn         = "arn:aws:iam::767397838496:role/ecsTaskExecutionRole"
   container_definitions = jsonencode([
     {
-      name: "wordpress",
-      image: "docker.io/wordpress:latest",
-      cpu: 0,
-      portMappings: [
+      "name": "wordpress",
+      "image": "docker.io/wordpress:latest",
+      "cpu": 0,
+      "portMappings": [
         {
-          name: "wordpress-80-tcp",
-          containerPort: 80,
-          hostPort: 80,
-          protocol: "tcp",
-          appProtocol: "http"
+          "name": "wordpress-80-tcp",
+          "containerPort": 80,
+          "hostPort": 80,
+          "protocol": "tcp",
+          "appProtocol": "http"
         }
       ],
-      essential: true,
-      environment: [],
-      environmentFiles: [],
-      mountPoints: [],
-      volumesFrom: [],
-      ulimits: [],
-      logConfiguration: {
-        logDriver: "awslogs",
-        options: {
+      "essential": true,
+      "environment": [],
+      "environmentFiles": [],
+      "mountPoints": [],
+      "volumesFrom": [],
+      "ulimits": [],
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
           "awslogs-group": "/ecs/application",
           "awslogs-create-group": "true",
-          "awslogs-region": "us-east-2",
+          "awslogs-region": var.region,  # Reference the 'region' variable
           "awslogs-stream-prefix": "ecs"
         },
-        secretOptions: []
+        "secretOptions": []
       },
-      systemControls: []
+      "systemControls": []
     }
   ])
 }
